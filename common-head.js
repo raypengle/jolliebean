17eea45723114782dd09ff15e4dec27ac2eb8d17
Monterry <138436595+raypengle@users.noreply.github.com>
2026-07-16 13:14:46 +0800

    Add files via upload

---
+++ common-head.js
@@ -0,0 +1,8 @@
+(function () {
+  var title = window.pageTitle || '欢乐豆工作室';
+  document.write('<meta charset="UTF-8">');
+  document.write('<title>' + title + '</title>');
+  document.write('<link rel="icon" type="image/png" href="./images/icon_web.png">');
+  document.write('<link rel="stylesheet" type="text/css" href="style.css">');
+  document.write('<!-- <script src="script.js"></script> -->');
+})();
---
+++ common-header.js
@@ -0,0 +1,16 @@
+(function () {
+  document.write('<header>');
+  document.write('  <div class="logo">');
+  document.write('    <a href="index.html" aria-label="返回主页">');
+  document.write('      <img src="./images/logo.png" alt="欢乐豆工作室">');
+  document.write('    </a>');
+  document.write('  </div>');
+  document.write('  <nav>');
+  document.write('    <ul>');
+  document.write('      <li><a href="index.html">主页</a></li>');
+  document.write('      <li><a href="donate.html">赞助</a></li>');
+  document.write('      <li><a href="store.html">商城</a></li>');
+  document.write('    </ul>');
+  document.write('  </nav>');
+  document.write('</header>');
+})();
