7780da90c3855b48ae7cf5fae4382cfa0b717c61
raypengle <2367358808@qq.com>
2026-07-16 13:10:49 +0800

    demo_1.1.0

--- common-header.js
+++ common-header.js
@@ -2,15 +2,13 @@
   document.write('<header>');
   document.write('  <div class="logo">');
   document.write('    <a href="index.html" aria-label="返回主页">');
-  document.write('      <img src="logo.png" alt="欢乐豆工作室">');
+  document.write('      <img src="./images/logo.png" alt="欢乐豆工作室">');
   document.write('    </a>');
   document.write('  </div>');
   document.write('  <nav>');
   document.write('    <ul>');
   document.write('      <li><a href="index.html">主页</a></li>');
-  document.write('      <li><a href="latestvideo.html">最新视频</a></li>');
   document.write('      <li><a href="donate.html">赞助</a></li>');
-  document.write('      <li><a href="about.html">关于</a></li>');
   document.write('      <li><a href="store.html">商城</a></li>');
   document.write('    </ul>');
   document.write('  </nav>');
